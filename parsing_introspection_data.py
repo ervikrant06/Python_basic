2de8184f5fb95f497c20ee6afdd906c304000b41
ervikrant06 <ervikrant06@gmail.com>
2017-02-02 22:44:49 +0530

    Added the docstring

--- parsing_introspection_data.py
+++ parsing_introspection_data.py
@@ -5,13 +5,23 @@ import enum
 from prettytable import PrettyTable
 x=PrettyTable()
 
+### ====================================================== ###
+
+# sub-sub-keys for which value needs to be print from extra dictionary sub-key.
+
 cpu_list=['physicalcpu','clock','cores','enabled_cores','frequency','physid','product','threads','vendor','version']
 ipmi_list=['802.1q-vlan-id','802.1q-vlan-priority','default-gateway-ip','ip-address','ip-address-source','mac-address','subnet-mask','rmcp+-cipher-suites']
 network_list=['iface','driver','firmware','serial','link','product','duplex','speed','vendor']
 memory_list=['description','product','size','slot','vendor','clock']
 disk_list=['disk_name','model','size','vendor','SMART/serial_number','wwn-id']
 
+### ======================================================= ###
+
 with open(sys.argv[1]) as data_file:
+    '''
+    Reading introspection json file which we are going to pass as first and
+    only argument to this python script.
+    '''
     data=json.load(data_file)
     options=dict()
     for index,value in enumerate(sorted(data.keys()),1):
@@ -19,6 +29,10 @@ with open(sys.argv[1]) as data_file:
         print("{0}: {1}".format(index,value))
 
 def display(inputlistarguments):
+    '''
+    If chosen option doesn't require sub-menu then display the results for
+    options chosen from main-menu.
+    '''
     if type(data[options[inputlistarguments]]) == dict:
         x.field_names=data[options[inputlistarguments]].keys()
         x.add_row([eachvalue for eachvalue in data[options[inputlistarguments]].values()])
@@ -27,6 +41,11 @@ def display(inputlistarguments):
         print(data[options[inputlistarguments]])
 
 def extra_options_print(input_main_key,input_sub_key):
+    '''
+    If "extra" is choosen from main menu then that key is input_main_key
+    and sub-key choosen is input_sub_key. Depending upon sub-key values
+    corresponding to sub-sub-keys are displayed.
+    '''
     if input_sub_key == 'cpu':
         x.field_names=cpu_list
         for keys in sorted(data[input_main_key][input_sub_key].keys()):
@@ -76,6 +95,10 @@ def extra_options_print(input_main_key,input_sub_key):
         print(x.get_string())
 
 def inventory_options_print(input_main_key,input_sub_key):
+    '''
+    input_main_key is inventory and input_sub_key is chosen from sub-menu of
+    inventory. Depending up-on type of sub-key display the results.
+    '''
     if type(data[input_main_key][input_sub_key]) == list:
         x.field_names=data[input_main_key][input_sub_key][0].keys()
         for eachelement in data[input_main_key][input_sub_key]:
@@ -90,6 +113,10 @@ def inventory_options_print(input_main_key,input_sub_key):
         print(data[input_main_key][input_sub_key])
 
 def another_menu(input_values1):
+    '''
+    Display the sub-keys of chosen menu option. Call separate function once
+    sub-option is choose.
+    '''
     extra_options=dict()
     for index_extra,value_extra in enumerate(sorted(data[input_values1].keys()),1):
         extra_options[index_extra]=value_extra
@@ -100,16 +127,21 @@ def another_menu(input_values1):
         extra_options_print(input_values1,extra_options[int(input("Enter one numeric value: "))])
 
 def check(input_values):
+    '''
+    If chosen option is extra or inventory then display the sub-menu
+    accordingly.
+    '''
     if (options[input_values] == 'extra' or options[input_values] == 'inventory'):
         another_menu(options[input_values])
     else:
         display(input_values)
 
 def input_validation():
+    '''
+    Main menu input from user and check whether it requires nested menu or
+    not.
+    '''
     inputvalues=str(input("Enter one numeric value: "))
-    #if set(inputvalues).issubset(set(options.keys())):
     check(int(inputvalues))
-    #else:
-    #    raise('One or more input values are invalid')
 
 input_validation()
